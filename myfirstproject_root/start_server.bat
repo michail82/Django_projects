3d5be30dfae963c85eb80be694f2da59a555c185
Michail <51780751+michail82@users.noreply.github.com>
2020-08-08 21:28:04 +0200

    Initial commit

---
+++ myfirstproject_root/start_server.bat
@@ -0,0 +1,2 @@
+cd d:\GoogleDisk\Programming\Python\Udemy_Python+Django\Django_projects\myfirstproject_root\
+python manage.py runserver
---
+++ vaseversa_project/start_server_versa.bat
@@ -0,0 +1,2 @@
+cd d:\GoogleDisk\Programming\Python\Udemy_Python+Django\Django_projects\vaseversa_project\
+python manage.py runserver
